3be395916c853e28dc41c33c95504949ba3d3bc2
ManitAwasthi <manitawasthi@gmail.com>
2026-02-08 11:58:03 +0530

    Update Q9.C

--- EXP 1/Q9.C
+++ EXP 1/Q9.C
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+struct S {
+    int i;
+    float f;
+    char c;
+};
+
+union U {
+    int i;
+    float f;
+    char c;
+};
+
+int main() {
+    printf("Structure size = %lu", sizeof(struct S));
+    printf("\nUnion size = %lu", sizeof(union U));
+}
+
+OUTPUT:
+Structure size = 12
+Union size = 4
+
+=== Code Execution Successful ===
