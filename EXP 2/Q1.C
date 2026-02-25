cdd10ee2bb1644f72959406c70afd18553f1c271
ManitAwasthi <manitawasthi@gmail.com>
2026-02-25 16:27:06 +0530

    Implement linked list and count nodes function
    Added a linked list implementation to count nodes.

--- EXP 2/Q1.C
+++ EXP 2/Q1.C
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node {
+    int data;
+    struct node* next;
+};
+
+int countNodes(struct node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+int main() {
+    struct node *head = NULL, *temp, *newnode;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+
+        if(head == NULL)
+            head = temp = newnode;
+        else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    printf("Number of nodes = %d\n", countNodes(head));
+    return 0;
+}
+
+Enter number of nodes: 3
+Enter data: 10
+Enter data: 20
+Enter data: 30
+Number of nodes = 3
